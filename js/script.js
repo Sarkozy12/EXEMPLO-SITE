6306b5393d9105893d500d95bc8c86b8da7417ac
Sarkozy12 <107371949+Sarkozy12@users.noreply.github.com>
2023-02-24 21:38:45 -0300

    Entrega aula 3 - WEB

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-alert("Olá!")
+alert("Cálculo de Aumento de Salário por Porcentagem")
+
+let salario = Number(prompt("Digite o valor do salário: "))
+let porc = Number(prompt("Digite a porcentagem de aumento (utilizando um número inteiro e sem o símbolo '%')"))
+
+let salarioFinal = salario * (1+(porc/100))
+
+document.write(salarioFinal)
